bc108a353a08ed67093d76854b5dc4ef4d92da3c
phil-hardwick <phil_hardwick@hotmail.co.uk>
2015-11-11 20:11:29 +0000

    Missed commits

--- src/test/java/com/capgemini/example/routes/RouteTest.java
+++ src/test/java/com/capgemini/example/routes/RouteTest.java
@@ -1,23 +1,39 @@
 package com.capgemini.example.routes;
 
-import com.capgemini.example.model.blog.Post;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.Dictionary;
 
+import static org.hamcrest.CoreMatchers.containsString;
 
+public class RouteTest extends CamelBlueprintTestSupport {
+
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "OSGI-INF/blueprint/blueprint.xml";
+    }
+
+    @Override
+    protected String useOverridePropertiesWithConfigAdmin(Dictionary props) throws Exception {
+        props.put("apiRootUrl", "http://localhost:9999/api");
+        return "com.capgemini.example";
+    }
+
+    @Test
+    public void testRoute() throws Exception {
+        HttpGet getRequest = new HttpGet("http://localhost:9999/api/blog/1");
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpResponse response = httpClient.execute(getRequest);
+
+        String responseBody = EntityUtils.toString(response.getEntity());
+        assertThat(responseBody, containsString("title"));
+    }
+
+}
